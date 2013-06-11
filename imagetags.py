ff86175f0856d169e2a02e1aacae1e44072e935c
Scott Torborg <scott@cartlogic.com>
2013-06-11 10:40:58 -0700

    Add script for building image tags

---
+++ imagetags.py
@@ -0,0 +1,12 @@
+import glob
+
+
+tmpl = '''
+<a class="image image-bordered" href="%(title)s.jpg">
+  <img src="%(filename)s" alt="" title="">
+</a>'''
+
+
+for fn in glob.glob('teardown/*-tiny.jpg'):
+    title = fn.split('.')[0].rsplit('-', 1)[0]
+    print tmpl % dict(title=title, filename=fn)
